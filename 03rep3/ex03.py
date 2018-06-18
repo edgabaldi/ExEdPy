a20acaaed765104580357a5342f6df3bdf7e6a46
cesardimi <39436343+cesardimi@users.noreply.github.com>
2018-06-18 15:28:47 -0300

    resolvido

---
+++ 03rep3/ex03.py
@@ -0,0 +1,5 @@
+''' 3º - Sabendo-se que as próximas Olimpíadas serão em 2012, projete um algoritmo que
+calcule e imprima todos os anos que terá Olimpíadas até 2060 '''
+
+for c in range(2012, 2061, 4):
+    print(c)
