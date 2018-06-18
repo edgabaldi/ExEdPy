d0cc56484d698aaf1c9a34326af6b1a0efee891f
cesardimi <39436343+cesardimi@users.noreply.github.com>
2018-06-18 15:24:21 -0300

    resolvido

---
+++ 03rep3/ex02.py
@@ -0,0 +1,5 @@
+'''2º - Sabendo-se que a próxima Copa do Mundo será em 2014, projete um algoritmo
+que calcule e imprima todos os anos que terá Copa do Mundo até 2050. '''
+
+for c in range(2014, 2051, 4):
+    print(c)
