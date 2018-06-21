18618eb8339cf894ea438487ddc9074d019a0eab
edgabaldi <edgabaldi@gmail.com>
2018-06-21 17:39:57 -0300

    starting tests :-)

---
+++ 01seq/README.md
@@ -0,0 +1,11 @@
+### Rodando testes com Doctest
+
+```
+$ python3 -m doctest ex01.py
+```
+
+### Rodando teste com asserção
+
+```
+$ python3 ex02.py
+```
--- 01seq/ex01.py
+++ 01seq/ex01.py
@@ -1,9 +1,11 @@
 ''' ler dois números, calcular a soma e o produto, mostrar os números, a soma e o produto'''
 
 
-n1 = float(input('nº: '))
-n2 = float(input('nº: '))
-
-print(f'Os números digitados foram {n1} e {n2}')
-print(f'{n1} + {n2} = {n1 + n2}')
-print(f'{n1} x {n2} = {n1 + n2}')
+def soma_produto(a, b):
+    """
+    >>> soma_produto(2, 4)
+    (6, 7)
+    """
+    soma = a + b
+    produto = a * b
+    return soma, produto
